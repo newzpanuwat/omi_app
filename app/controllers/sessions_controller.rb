b0993bde2103a954633cf55c37088f1932052676
newzpanuwat <new_sbh@me.com>
2017-07-27 21:03:44 +0700

    add more auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
-  def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
-    self.current_user = @user
-    redirect_to '/'
-  end
+    def create
+        auth = request.env["omniauth.auth"]
+        session[:omniauth] = auth.except('extra')
+        user = User.sign_in_from_omniauth(auth)
+        session[:user_id] = user.id
+        redirect_to root_url, notice: "Signed In"
+    end
 
-  protected
-
-  def auth_hash
-    request.env['omniauth.auth']
-  end
+    def destroy
+        session[:user_id] = nill
+        session[:omniauth] = nill
+        redirect_to root_url, notice "Sign Out"
+    end
 end
